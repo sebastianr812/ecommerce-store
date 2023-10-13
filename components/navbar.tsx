6dcbc8f0265e6afa7edf7b80357b4df244d3202a
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-10-13 12:50:32 -0400

    added: logo, changed footer to correct name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
+import Logo from "./Logo";
+import Image from "next/image";
 
 const Navbar = async () => {
 
@@ -13,7 +15,7 @@ const Navbar = async () => {
             <Container>
                 <div className="relative flex items-center h-16 px-4 sm:px-6 lg:px-8">
                     <Link href='/' className="flex ml-4 lg:ml-0 gap-x-2">
-                        <p className="text-xl font-bold">STORE</p>
+                        <p className="leading-none font-semibold text-2xl">Crown Clothing</p>
                     </Link>
                     <MainNav data={categories} />
                     <NavbarActions />
