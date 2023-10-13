cfcb9c72d39f0e7a73d7684de351198067762721
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-10-13 13:04:34 -0400

    fix: navbar added logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
+import crownLogo from "@/public/logos/crownClothing.svg";
+import Image from "next/image";
 
 const Navbar = async () => {
 
@@ -12,8 +14,9 @@ const Navbar = async () => {
         <div className="border-b">
             <Container>
                 <div className="relative flex items-center h-16 px-4 sm:px-6 lg:px-8">
-                    <Link href='/' className="flex ml-4 lg:ml-0 gap-x-2">
-                        <p className="leading-none font-semibold text-2xl">Crown Clothing</p>
+                    <Link href='/' className="flex ml-4 lg:ml-0 gap-x-2 items-center justify-center">
+                        <Image src={crownLogo} alt="logo" className="h-10 w-10" /> 
+                        <p className="leading-none font-semibold text-md">Crown Clothing</p>
                     </Link>
                     <MainNav data={categories} />
                     <NavbarActions />
